11ad0b84d406e83f064b1b4f085ec8cdb07b43d7
Gia Dat Truong <giadattruong@192.168.2.4>
2023-12-04 23:11:42 +0700

    feat: create card

---
+++ components/form/form-textarea.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { ComponentProps, forwardRef } from "react";
+import { useFormStatus } from "react-dom";
+
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { FormErrors } from "./form-errors";
+
+interface FormTextareaProps extends ComponentProps<"textarea"> {
+   id: string;
+   label?: string;
+   errors?: Record<string, string[] | undefined>;
+}
+
+export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
+   (
+      {
+         id,
+         label,
+         errors,
+         placeholder,
+         required,
+         disabled,
+         className,
+         onBlur,
+         onClick,
+         onKeyDown,
+         defaultValue,
+      },
+      ref
+   ) => {
+      const { pending } = useFormStatus();
+      return (
+         <div className="space-y-2 w-full">
+            <div className="space-y-1 w-full">
+               {label && (
+                  <Label htmlFor={id} className="text-xs font-semibold text-neutral-700">
+                     {label}
+                  </Label>
+               )}
+               <Textarea
+                  onKeyDown={onKeyDown}
+                  onBlur={onBlur}
+                  onClick={onClick}
+                  ref={ref}
+                  required={required}
+                  placeholder={placeholder}
+                  name={id}
+                  id={id}
+                  disabled={pending || disabled}
+                  className={cn(
+                     "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 focus:ring-0 outline-none shadow-sm",
+                     className
+                  )}
+                  aria-describedby={`${id}-error`}
+                  defaultValue={defaultValue}
+               />
+            </div>
+            <FormErrors id={id} errors={errors} />
+         </div>
+      );
+   }
+);
+
+FormTextarea.displayName = "FormTextarea";
